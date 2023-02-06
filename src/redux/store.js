44b432d0ce057c326bec4c62aacbe53ba55c4858
TranLeHaiDuong <haiduongtlh2000@gmail.com>
2023-02-06 14:32:25 +0700

    admin

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,42 +1,38 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import userReducer from './userRedux'
+import productReducer from './productRedux'
 import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import user from "./slices/userSlice";
 const persistConfig = {
-  key: "root",
-  version: 1,
-  storage,
+    key: "root",
+    version: 1,
+    storage,
 };
 
-const rootReducer = combineReducers({ user });
+const rootReducer = combineReducers(
+    {
+        user: userReducer, 
+        product: productReducer
+    });
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-export const storePersist = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-});
-
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-});
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+})
 
-export let persistor = persistStore(storePersist);
+export let persistor = persistStore(store);
