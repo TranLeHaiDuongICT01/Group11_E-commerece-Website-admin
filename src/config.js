cee85e9c4e3cee7e042f0d9fb5d03580ba7a4f91
TranLeHaiDuong <haiduongtlh2000@gmail.com>
2023-02-06 15:50:31 +0700

    admin

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL =
-  'https://group11-e-commerece-website-server.vercel.app/api';
+export const BASE_URL = 'http://localhost:5000/api';
 // const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZHVvbmciLCJlbWFpbCI6ImR1b25nQGdtYWlsLmNvbSIsImlkIjoiNjJhYzllZmQ1MTI1NmM5ZTUwNzlmMjgyIiwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjU1NTUzNDI0LCJleHAiOjE2NTU1NjQyMjR9.oMh1m9p3Qx4GBoJpNjHJ7URhu0cgEaaeV9lyu8zyNDo"
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
